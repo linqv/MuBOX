7f50a1b1dae8d36a644a1dd8a5ee0d751471ff91
lin <q19212@outlook.com>
2026-05-28 21:36:24 +0800

    chore: bump version to 1.0.2 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,8 +86,8 @@ android {
         applicationId = "org.mubox.reader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
